be6aeceb006983ba3383651c396e7b2351b7bd09
baybird <bayareabird@gmail.com>
2021-08-05 16:43:34 -0700

    clean up.

--- CS143/src/SortingSearching/SelectionSort.java
+++ CS143/src/SortingSearching/SelectionSort.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package SortingSearching;
 
 import java.util.*;
@@ -18,23 +13,9 @@ public class SelectionSort {
     // sorting methods in Java
     // Arrays & Collections classes in java.util
     public static void main(String[] args) {
+        // String[] elements = {"foo", "bar", "baz", "cat", "bee", "daa"};
         int[] numbers = {29, 17, 3, 94, 46, 8, -4, 12};
         selectionSort(numbers);
-    }
-
-    public static void javaSorts() {
-        String[] elements = {"foo", "bar", "baz", "cat", "bee", "daa"};
-
-        List<String> words = new ArrayList<String>();
-        for (String element : elements) {
-            words.add(element);
-        }
-
-        Arrays.sort(elements);
-        System.out.println(Arrays.toString(elements));
-
-        Collections.sort(words);
-        System.out.println(words);
     }
 
     public static void selectionSort(int[] items) {
@@ -50,7 +31,6 @@ public class SelectionSort {
 
             // swap smallest value to its proper place, a[i]
             swap(items, i, min);
-            
             System.out.println(Arrays.toString(items));
         }
 
